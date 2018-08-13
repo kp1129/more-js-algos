aada4824c5551bf46eadc230984016acd22e0d06
kp1129 <41562973+kp1129@users.noreply.github.com>
2018-08-13 16:50:47 -0700

    Create sort-array-alphabetically.js

---
+++ sort-array-alphabetically.js
@@ -0,0 +1,5 @@
+// Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
+
+function alphabeticalOrder(arr) {
+  return arr.sort((a, b) => a > b);
+}
