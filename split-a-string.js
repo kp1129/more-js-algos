e7426d5a5afc85463f653db7813dc63e0e57882a
kp1129 <41562973+kp1129@users.noreply.github.com>
2018-08-13 17:32:25 -0700

    Create split-a-string.js

---
+++ split-a-string.js
@@ -0,0 +1,10 @@
+/* Use the split method inside the splitify function to split str into an array of words. 
+The function should return the array. 
+
+Note that the words are not always separated by spaces, 
+and the array should not contain punctuation. */
+
+function splitify(str) {
+  return str.split(/[^a-zA-Z]/);
+}
+splitify("Hello World,I-am code");
